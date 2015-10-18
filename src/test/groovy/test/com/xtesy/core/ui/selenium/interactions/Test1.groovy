e0bc6cef1908bc824f667ed53af0ebb59ec51ab3
WasiqB <wasbhamla2005@gmail.com>
2015-10-18 19:29:01 +0530

    Test package added.

---
+++ src/test/groovy/test/com/xtesy/core/ui/selenium/interactions/Test1.groovy
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2015, Wasiq Amjad Bhamla.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test.com.xtesy.core.ui.selenium.interactions
+
+import org.testng.annotations.Test
+
+/**
+ * @author Wasiq B
+ * @since 18-Oct-2015 7:26:41 pm
+ */
+class Test1 {
+	@Test
+	void test1 () {
+		assert true
+	}
+}
